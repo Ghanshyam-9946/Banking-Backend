8e0cb1bdd9f606616b5e89b8b68d2be24c21b8a7
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-03-05 11:32:37 +0530

    fix cors for vercel frontend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');   
+const cors = require('cors');
 
 const authRouter = require('./routes/auth.route');
 const accountRouter = require('./routes/account.routes');
@@ -8,21 +8,29 @@ const transactionRouter = require('./routes/transaction.routes');
 
 const app = express();
 
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://banking-frontend-green.vercel.app"
+];
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://banking-frontend-green.vercel.app"
-  ],
-  credentials: true
-}))
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Banking API");
+  res.send("Welcome to the Banking API");
 });
 
 app.use('/api/auth', authRouter);
