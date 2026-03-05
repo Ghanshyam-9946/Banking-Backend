0bc3a4f1ccca722c38d2c7921ad228341595303b
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-03-05 16:32:40 +0530

    increase smtp timeout

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   requireTLS: true,
+
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
+
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 // Verify transporter connection
