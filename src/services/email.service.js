5911e4f300b37f545144a333ce0da3e4403c21f2
Ghanshyam-9946 <dhoteghanshyam9@gmail.com>
2026-03-05 16:16:08 +0530

    Fix SMTP config

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-// Transporter setup using Gmail App Password
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 // Verify transporter connection
